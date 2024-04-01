c426bfa668a3186ee9250b96b8c7f93088977b62
davimci-code <davimcicode@outlook.com>
2024-04-01 13:14:14 -0400

    addition of a new project

---
+++ programs/solana-global-article/src/lib.rs
@@ -0,0 +1,21 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod solana_global_article {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+
+#[account]
+pub struct Article {
+    pub content: String,
+}
